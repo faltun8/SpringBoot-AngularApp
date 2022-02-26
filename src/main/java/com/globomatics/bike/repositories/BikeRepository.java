a006b8238d81d560e4e2c38af8747cce3dc67b68
Furkan Altun <faltun@architech.ca>
2022-02-25 22:23:01 -0500

    moved backend in a folder

--- src/main/java/com/globomatics/bike/repositories/BikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.globomatics.bike.repositories;
-
-import com.globomatics.bike.models.Bike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BikeRepository extends JpaRepository <Bike, Long> {
-}
